93f52832058ee0ff8c92fd69d455a8bce18a3038
BobrikovMikhail <mihapir4t@yandex.ru>
2024-11-20 22:51:26 +0500

    Исправление визуала в меню операций

--- SubMenu.xaml.cs
+++ SubMenu.xaml.cs
@@ -33,8 +33,17 @@ namespace WpfLab1
             TransferLabelX.Visibility = Visibility.Visible;
             TransferY.Visibility = Visibility.Visible;
             ScaleBtn.Background = Brushes.LightGray;
+            RolateBtn.Background = Brushes.LightGray;
+            MirrorSC.Background = Brushes.LightGray;
+            MirrorX.Background = Brushes.LightGray;
+            MirrorY.Background = Brushes.LightGray;
             TransferCofirmBtn.Visibility = Visibility.Visible;
             ScaleConfBtn.Visibility = Visibility.Hidden;
+            RolateConfBtn.Visibility = Visibility.Hidden;
+            ScaleX.Visibility = Visibility.Hidden;
+            ScaleY.Visibility = Visibility.Hidden;
+            RolateTb.Visibility = Visibility.Hidden;
+
 
 
         }
@@ -56,11 +65,19 @@ namespace WpfLab1
             ScaleX.Visibility = Visibility.Visible;
             ScaleY.Visibility = Visibility.Visible;
             Transfer.Background = Brushes.LightGray;
+            RolateBtn.Background = Brushes.LightGray;
+            MirrorSC.Background = Brushes.LightGray;
+            MirrorX.Background = Brushes.LightGray;
+            MirrorY.Background = Brushes.LightGray;
             TransferX.Visibility = Visibility.Hidden;
             TransferLabelY.Visibility = Visibility.Hidden;
             TransferLabelX.Visibility = Visibility.Hidden;
             TransferY.Visibility = Visibility.Hidden;
             TransferCofirmBtn.Visibility = Visibility.Hidden;
+            ScaleConfBtn.Visibility = Visibility.Hidden;
+            RolateConfBtn.Visibility = Visibility.Hidden;
+            ScaleConfBtn.Visibility = Visibility.Visible;
+            RolateTb.Visibility = Visibility.Hidden;
 
         }
 
@@ -72,21 +89,51 @@ namespace WpfLab1
         private void MirrorX_Click(object sender, RoutedEventArgs e)
         {
             MainWindow.MirrorX();
+            MirrorX.Background = Brushes.Green;
+            MirrorY.Background = Brushes.LightGray;
+            MirrorSC.Background = Brushes.LightGray;
         }
 
         private void MirrorY_Click(object sender, RoutedEventArgs e)
         {
             MainWindow.MirrorY();
+            MirrorY.Background = Brushes.Green;
+            MirrorSC.Background = Brushes.LightGray;
+            MirrorX.Background = Brushes.LightGray;
         }
 
         private void MirrorSC_Click(object sender, RoutedEventArgs e)
         {
             MainWindow.MirroSC();
+            MirrorSC.Background = Brushes.Green;
+            MirrorY.Background = Brushes.LightGray;
+            MirrorX.Background = Brushes.LightGray;
         }
 
         private void RolateBtn_Click(object sender, RoutedEventArgs e)
         {
            
+            RolateTb.Visibility = Visibility.Visible;
+            RolateBtn.Background = Brushes.Green;
+            RolateConfBtn.Visibility = Visibility.Visible;
+
+            //убираем видимость элементов других операций
+            Transfer.Background = Brushes.LightGray;
+            ScaleBtn.Background = Brushes.LightGray;
+            MirrorSC.Background = Brushes.LightGray;
+            MirrorX.Background = Brushes.LightGray;
+            MirrorY.Background = Brushes.LightGray;
+
+            TransferX.Visibility = Visibility.Hidden;
+            TransferLabelY.Visibility = Visibility.Hidden;
+            TransferLabelX.Visibility = Visibility.Hidden;
+            TransferY.Visibility = Visibility.Hidden;
+            TransferCofirmBtn.Visibility = Visibility.Hidden;
+            ScaleConfBtn.Visibility = Visibility.Hidden;
+            ScaleConfBtn.Visibility = Visibility.Hidden;
+            ScaleX.Visibility = Visibility.Hidden;
+            ScaleY.Visibility = Visibility.Hidden;
+
         }
 
         private void RolateConfBtn_Click(object sender, RoutedEventArgs e)
